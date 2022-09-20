85d0c5ed2b5a80f9463880f3623eaedf561e7f70
Rob Anderson <robanderson@hey.com>
2022-09-20 17:16:09 +0100

    experimenting

--- experiments/site-wizard/build-shallow.js
+++ experiments/site-wizard/build-shallow.js
@@ -8,7 +8,7 @@ import { createServer } from "http";
 import { promisify } from "util";
 import cp from "child_process";
 
-// import dedent from "dedent";
+import dedent from "dedent";
 import serveHandler from "serve-handler";
 import { Parcel } from "@parcel/core";
 import { globby } from "globby";
@@ -16,6 +16,7 @@ import nunjucks from "nunjucks";
 import minio from "minio";
 
 import { getConfig } from "./config.js";
+import { find } from "./json-xpath.js";
 
 const {
   NODE_ENV = "production",
@@ -26,15 +27,8 @@ const {
 } = process.env;
 const exec = promisify(cp.exec);
 
-/** @param {string} input */
-function safeJson(input) {
-  return input.replaceAll(/'/g, "\\'").replaceAll(/\\n/g, "\\\\n");
-}
-
 // TODO:
-// - fontawesome stuff
 // - embed SVG icons
-// - work out how to do assets from CDN/uploads
 
 //
 // Configuration that should be passed to ths wizard as parameters
@@ -49,6 +43,91 @@ const s3 = new minio.Client({
   accessKey: S3_ACCESS_KEY,
   secretKey: S3_SECRET_KEY,
 });
+
+/** @param {Map<string, [string,string]>} faIcons */
+function createIconsJs(faIcons) {
+  const importUrls = {
+    fas: "@fortawesome/free-solid-svg-icons",
+    far: "@fortawesome/free-regular-svg-icons",
+    fab: "@fortawesome/free-brands-svg-icons",
+  };
+  const icons = Array.from(faIcons.values());
+
+  /** @type {Record<string, Set<string>>}  */
+  const usage = {
+    fas: new Set(),
+    far: new Set(),
+    fab: new Set(),
+  };
+  for (const icon of icons) {
+    const set = usage[icon[0]];
+    if (!set) throw new Error(`Unknown icon set '${icon[0]}'`);
+    set.add(icon[1]);
+  }
+
+  const prefix = dedent`
+    import { library } from '@fortawesome/fontawesome-svg-core'
+    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+  `;
+  const suffix = dedent`
+    export class FontawesomePlugin {
+      static install(Vue) {
+        Vue.component('fa-icon', FontAwesomeIcon);
+      }
+    }
+  `;
+
+  /** @param {string} i */
+  const iconName = (i) => {
+    const parts = i
+      .split(/-+/)
+      .map((p) => p.slice(0, 1).toLocaleUpperCase() + p.slice(1));
+    return "fa" + parts.join("");
+  };
+
+  const imports = Object.entries(usage)
+    .filter((entry) => entry[1].size > 0)
+    .map(
+      ([kind, icons]) =>
+        dedent`
+          import { ${[...icons].map((i) => iconName(i)).join(", ")} } from "${
+          importUrls[kind]
+        }";
+        `
+    )
+    .join("\n");
+
+  // const additions = `library.add(${Object.values(usage).flatMap()})`
+  const libraryAdditions = dedent`
+    library.add(${Object.values(usage)
+      .flatMap((s) => Array.from(s))
+      .map((i) => iconName(i))
+      .join(", ")})
+    `;
+
+  return [prefix, imports, libraryAdditions, suffix].join("\n\n");
+}
+function createConfigJs(config) {
+  return `window.DECONF_CONFIG = JSON.parse(${JSON.stringify(
+    JSON.stringify(config)
+  )});`;
+}
+
+const assetPaths = [
+  "/site/customScripts/*",
+  "/site/customStyles/*",
+  "/site/opengraph/image",
+  "/navigation/*/icon",
+  "/pages/*/home/sponsors/*/sponsors/*/image",
+];
+
+const iconPaths = [
+  "/pages/*/home/widgets/*/builtin/faIcon",
+  "/pages/*/home/widgets/*/page/faIcon",
+  "/pages/*/home/widgets/*/url/faIcon",
+  "/taxonomies/*/faIcon",
+  "/taxonomies/*/options/*/faIcon",
+];
 
 async function main() {
   const config = await getConfig();
@@ -70,29 +149,16 @@ async function main() {
 
   try {
     // 2. Prep assets
-    for (const script of config.site.customScripts) await cacheObject(script);
-    for (const style of config.site.customStyles) await cacheObject(style);
-    if (config.site.opengraph.image) {
-      await cacheObject(config.site.opengraph.image);
-    }
-    for (const item of config.navigation) await cacheObject(item.icon);
-    for (const page of config.pages) {
-      if (page.type === "home") {
-        for (const group of page.home.sponsors) {
-          for (const sponsor of group.sponsors) {
-            await cacheObject(sponsor.image);
-          }
-        }
-      }
-    }
+    const allAssets = assetPaths.flatMap((p) => find(config, p));
+    const faIcons = iconPaths.flatMap((p) => find(config, p));
+
+    await Promise.all(
+      Array.from(new Set(allAssets)).map((p) => cacheObject(p))
+    );
 
     // 3. Process the template
-    await fs.writeFile(
-      path.join(tmpdir, "config.js"),
-      `window.DECONF_CONFIG = JSON.parse('${safeJson(
-        JSON.stringify(config)
-      )}');`
-    );
+    await fs.writeFile(path.join(tmpdir, "config.js"), createConfigJs(config));
+    await fs.writeFile(path.join(tmpdir, "icons.js"), createIconsJs(faIcons));
 
     const env = new nunjucks.Environment(new nunjucks.FileSystemLoader());
     const context = { ...config };
---
+++ experiments/site-wizard/json-xpath.js
@@ -0,0 +1,70 @@
+import { getConfig } from "./config.js";
+
+export function find(object, path) {
+  // If a string was passed, validate it and call again with the array versuib
+  if (!Array.isArray(path)) {
+    if (typeof path !== "string" || !path.startsWith("/")) {
+      throw new Error("Invalid path");
+    }
+    return find(object, path.split("/").slice(1));
+  }
+
+  // Get the current bit of the path to parse
+  const [head, ...tail] = path;
+
+  // If at the end of the path...
+  if (path.length === 1) {
+    // Return cloned values if ending in a wildcard
+    if (head === "*") return Array.isArray(object) ? Array.from(object) : [];
+
+    // Or the object value if it is defined
+    const value = object?.[head];
+    return value !== undefined ? [value] : [];
+  }
+
+  // Recursively process wildcards
+  if (head === "*") {
+    return Array.isArray(object)
+      ? object.flatMap((nested) => find(nested, tail))
+      : [];
+  }
+
+  // If a valid object is next, recurse into it with the remainding path
+  const value = object?.[head];
+  if (typeof value === "object" && object !== null) {
+    return find(value, tail);
+  }
+
+  // If not found, return no results
+  return [];
+}
+
+export async function testFn() {
+  const config = await getConfig();
+
+  const testPaths = [
+    "/site/customScripts/*",
+    "/site/customStyles/*",
+    "/site/opengraph/image",
+    "/navigation/*/icon",
+    "/pages/*/home/sponsors/*/sponsors/*/image",
+
+    // ...
+
+    "/pages/*/home/widgets/*/builtin/faIcon",
+    "/pages/*/home/widgets/*/page/faIcon",
+    "/pages/*/home/widgets/*/url/faIcon",
+    "/taxonomies/*/faIcon",
+    "/taxonomies/*/options/*/faIcon",
+
+    // ... misc
+
+    "/pages/*/home/sponsors/*/sponsors/1/image",
+  ];
+
+  for (const path of testPaths) {
+    console.log(path, find(config, path));
+  }
+}
+
+// testFn();
